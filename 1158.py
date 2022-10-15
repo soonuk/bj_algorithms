9e796dad4fd15f73a3aafa26b5be1a85f9508da5
soonuk <kswnuguri@gmail.com>
2022-10-15 22:14:01 +0900

    221015 commit

--- 1158.py
+++ 1158.py
@@ -1,6 +1,6 @@
 import sys
 
-li=[]
+
 #insert값 0번==n, 1번 ==k
 ins = list(map(int, sys.stdin.readline().split()))
 ran = range(1,ins[0])
@@ -9,17 +9,18 @@ pos =0
 ans = []
 while True:
     pos+=ins[1]
-    if len(circle) <= ins[1]:
-        ans.append(ran.pop())
-    elif pos>len(circle):
+    if len(circle)<ins[1]:
+        
+
+    if pos>len(circle):
         pos-=len(circle)
 
-        transli1 =circle[0:pos]
-        transli2 = circle[pos:]
-        ans.append(transli1.pop())
-        pos-=1
-        circle= transli1 + transli2
-    else:
+    transli1 =circle[0:pos]
+    transli2 = circle[pos:]
+    ans.append(transli1.pop())
+    pos-=1
+    circle= transli1 + transli2
+    
         
     if circle ==[]:
         break
